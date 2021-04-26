3f58c3337b40fcdea429f5bfbb67c2c348d420f9
Piyush Badule <piyush.badule30@gmail.com>
2021-04-26 10:31:54 +0530

    Anagram

---
+++ Anagram.py
@@ -0,0 +1,10 @@
+a='this is bad but Dad to listen being silent'
+b='';d=0
+for x in range (0,len(a)):
+    if (a[x]==" "):
+        for y in range (len(b)):
+            sorted(b[y])
+            print(b)
+    else:
+        b=b+a[x]
+# print(b)
