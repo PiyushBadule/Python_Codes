33c5a6d50849245e5fade551b248532b91ddb6ad
Piyush Badule <piyush.badule30@gmail.com>
2021-05-01 08:59:14 +0530

    Making_Table_From_UserInput

--- any number table.py
+++ any number table.py
@@ -1,3 +1,5 @@
-number=int(input("Enter Number:"))
+number = int(input("Enter Number:"))
 for x in range (1,11):
-    print(number,"*",x,"=",(number*x))
+    print(number, "*", x, "=", (number*x))
+    # or
+    # print(f'{number} * {x} =', number*x)
