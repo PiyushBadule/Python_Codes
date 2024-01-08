80cf1c1eacbad872cad8a604fe2f8b3bf1e5cf2a
Piyush Badule <73275216+PiyushBadule@users.noreply.github.com>
2024-01-08 10:25:53 +0530

    Update Convert_UpperCasetoLower&VIseVersa.py

--- Convert_UpperCasetoLower&VIseVersa.py
+++ Convert_UpperCasetoLower&VIseVersa.py
@@ -1,10 +1,35 @@
-string=input("Enter the string:")
-newstring =''
-for a in string:
-    if (a.isupper()) == True:
-        newstring += (a.lower())
-    elif (a.islower()) == True:
-        newstring += (a.upper())
-    elif (a.isspace()) == True:
-        newstring += a
-print(newstring)
+def toggle_case(input_string):
+    """
+    Takes an input string and returns a new string with the case of each alphabetical character toggled.
+
+    Args:
+    input_string (str): The string to be modified.
+
+    Returns:
+    str: A new string with all alphabetical characters having their case toggled from the original.
+
+    Example:
+    >>> toggle_case("Hello World")
+    'hELLO wORLD'
+    """
+    new_string = ''
+    for char in input_string:
+        if char.isupper():
+            new_string += char.lower()
+        elif char.islower():
+            new_string += char.upper()
+        else:
+            new_string += char
+    return new_string
+
+def main():
+    """
+    Main function to execute script tasks.
+    """
+    # User input
+    input_string = input("Enter the string: ")
+    # Manipulate and print the new string
+    print(toggle_case(input_string))
+
+if __name__ == "__main__":
+    main()
