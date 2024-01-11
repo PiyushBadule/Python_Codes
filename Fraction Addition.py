0a67efa257adc3c92cd12af1f2d8f0a66e5bf668
Piyush Badule <73275216+PiyushBadule@users.noreply.github.com>
2024-01-11 08:45:07 +0530

    Update Fraction Addition.py

--- Fraction Addition.py
+++ Fraction Addition.py
@@ -1,29 +1,39 @@
-# Take input as 2 different Array and add them and as a fraction
-# Input: a = [2, 3]
-#        b = [1, 2]
-# Output: [7, 6]
+def add_fractions(fraction1, fraction2):
+    """
+    Add two fractions and return the result as a fraction.
 
-# Input: a = [0, 3]
-#        b = [0, 2]
-# Output: [0, 0]
+    Args:
+    fraction1 (list): A list of two integers representing the first fraction (numerator, denominator).
+    fraction2 (list): A list of two integers representing the second fraction (numerator, denominator).
 
-# Input: a = [1, 0]
-#        b = [3, 2]
-# Output: [3, 2]
-# Hint: {2/3 +1/2}
-# Note Handle Zero Condition as well
+    Returns:
+    list: A list of two integers representing the sum of the fractions.
+    """
+    # Extracting numerators and denominators
+    num1, den1 = fraction1
+    num2, den2 = fraction2
 
-a = [2, 3]
-b = [1, 2]
-if(a[1] == b[1]):
-    Fraction = a[0] + b[0]
-    print([Fraction, a[1]])
-elif 0 in a and 0 in b:
-    print([0,0])
-elif 0 in a:
-    print(b)
-elif 0 in b:
-    print(a)
-else:
-    Fraction = (a[0] * b[1]) + (a[1] * b[0])
-    print([Fraction, (a[1] * b[1])])
+    # Handling zero denominator cases
+    if den1 == 0 or den2 == 0:
+        return [0, 0]
+
+    # If denominators are the same
+    if den1 == den2:
+        return [num1 + num2, den1]
+
+    # If either numerator is zero
+    if num1 == 0:
+        return fraction2
+    if num2 == 0:
+        return fraction1
+
+    # General case: a/b + c/d = (ad + bc) / bd
+    new_num = (num1 * den2) + (num2 * den1)
+    new_den = den1 * den2
+    return [new_num, new_den]
+
+# Testing the function with different inputs
+if __name__ == "__main__":
+    print("Fraction addition [2, 3] + [1, 2]:", add_fractions([2, 3], [1, 2]))
+    print("Fraction addition [0, 3] + [0, 2]:", add_fractions([0, 3], [0, 2]))
+    print("Fraction addition [1, 0] + [3, 2]:", add_fractions([1, 0], [3, 2]))
