731b39e6ad27bccecaa5eff6d791b45feb237e8c
Piyush Badule <73275216+PiyushBadule@users.noreply.github.com>
2024-01-11 08:42:54 +0530

    Update Flatten_a_nested_array.py

--- Flatten_a_nested_array.py
+++ Flatten_a_nested_array.py
@@ -1,19 +1,27 @@
-# Input:- [1,2,[3],[[4]],[[[5]]]].
-# Output:- [1, 2, 3, 4, 5]
+def flatten(nested_list):
+    """
+    Flatten a nested list of lists into a single list.
 
-def flatten(list_of_lists):
-    print("def:- ", list_of_lists)
-    if len(list_of_lists) == 0:
-        print("First if:-", list_of_lists)
-        return list_of_lists
-    if isinstance(list_of_lists[0], list):
-        print("Second if", list_of_lists)
-        print("Second if Return", (list_of_lists[0])+ list_of_lists[1:])
-        return flatten(list_of_lists[0]) + flatten(list_of_lists[1:])
-    print("Last Return:-", list_of_lists[:1]
-          + list_of_lists[1:])
-    return list_of_lists[:1] + flatten(list_of_lists[1:])
+    Args:
+    nested_list (list): A list which can contain elements and/or other lists.
 
+    Returns:
+    list: A single, flat list with all the elements of the nested lists.
+    """
+    flat_list = []
+    for element in nested_list:
+        if isinstance(element, list):
+            # If the element is a list, extend the flat list with the flattened element
+            flat_list.extend(flatten(element))
+        else:
+            # If the element is not a list, append it to the flat list
+            flat_list.append(element)
+    return flat_list
 
-print(flatten([1, 2, [3], [[4]], [[[5]]]]))
+# Testing the function with an example
+if __name__ == "__main__":
+    nested_list = [1, 2, [3], [[4]], [[[5]]]]
+    print("Original list:", nested_list)
+    print("Flattened list:", flatten(nested_list))
+
 
