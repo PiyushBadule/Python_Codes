54fe8f882857a5a2553f1e5c0b10f37555890c2f
Piyush Badule <73275216+PiyushBadule@users.noreply.github.com>
2024-01-12 14:02:19 +0530

    Update MergeTwoString.py

--- MergeTwoString.py
+++ MergeTwoString.py
@@ -11,17 +11,37 @@
 #         b = "idowdk"
 # Output:- zibdxonwsdjkdns
 
-str1 = 'zbxnsjdns'
-str2 = 'idowdk'
+def merge_strings(str1, str2):
+    """
+    Merges two strings by alternating characters from each.
 
+    Args:
+    str1 (str): The first string to be merged.
+    str2 (str): The second string to be merged.
 
-def merge(str1, str2):
-    if len(str1) == 0:
-        return str1 + str2
-    return str1[:1] + str2[:1] + merge(str1[1:], str2[1:])
+    Returns:
+    str: A new string formed by alternating characters from str1 and str2. 
+         If one string is longer, the remaining characters are appended at the end.
+    """
+    merged = []
+    len_str1 = len(str1)
+    len_str2 = len(str2)
+    min_length = min(len_str1, len_str2)
 
+    for i in range(min_length):
+        merged.append(str1[i])
+        merged.append(str2[i])
 
-str1 = list(str1)
-str2 = list(str2)
-x = merge(str1, str2)
-print(''.join(x))
+    # Append the remaining characters of the longer string
+    if len_str1 > min_length:
+        merged.append(str1[min_length:])
+    elif len_str2 > min_length:
+        merged.append(str2[min_length:])
+
+    return ''.join(merged)
+
+if __name__ == "__main__":
+    str1 = 'zbxnsjdns'
+    str2 = 'idowdk'
+    result = merge_strings(str1, str2)
+    print(result)
