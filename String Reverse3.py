9d2f6e5011fcc780a6b4f3a9ab621f779aa18396
Piyush Badule <73275216+PiyushBadule@users.noreply.github.com>
2024-01-14 09:40:09 +0530

    String Reverse 3.py

--- String Reverse3.py
+++ String Reverse3.py
@@ -1,10 +1,36 @@
-a2 = " I am Piyush"
+# input:-  " I am Piyush"
 # output:- Piyush am I
-b2 = ''
-for x in a2[::-1]:
-    if x == " ":
-        print(b2[::-1], end=" ")
-        b2 = ""
-    else:
-        b2 = b2 + x
-print(b2)
+def reverse_word_order(input_string):
+    """
+    Reverses the order of words in the given string.
+
+    This function keeps the characters of each word intact but reverses the order of the words in the string.
+
+    Args:
+    input_string (str): The string whose word order is to be reversed.
+
+    Returns:
+    str: A string with the word order reversed.
+    """
+    reversed_word_order = []
+    word = ''
+    for char in input_string[::-1]:
+        if char == " ":
+            reversed_word_order.append(word)
+            word = ""
+        else:
+            word += char
+    reversed_word_order.append(word)  # Append the last word
+
+    return ' '.join(reversed_word_order[::-1])
+
+def main():
+    """
+    Main function to demonstrate the reversal of word order in a string.
+    """
+    input_string = "I am Piyush"
+    result = reverse_word_order(input_string)
+    print("Result:", result)
+
+if __name__ == "__main__":
+    main()
