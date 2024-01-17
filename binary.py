de446b96d3a60991698fc9851846590e3a97d8f8
Piyush Badule <73275216+PiyushBadule@users.noreply.github.com>
2024-01-17 08:32:34 +0530

    Update binary.py

--- binary.py
+++ binary.py
@@ -1,3 +1,25 @@
-i=int(input("Enter Number:"))
-b1="{0:b}".format(i)
-print(b1)
+def decimal_to_binary(number):
+    """
+    Convert a decimal number to its binary representation.
+
+    Parameters:
+    number (int): A decimal number.
+
+    Returns:
+    str: Binary representation of the decimal number.
+    """
+    return "{0:b}".format(number)
+
+def main():
+    """
+    Main function that prompts the user to enter a decimal number and prints its binary representation.
+    """
+    try:
+        decimal_number = int(input("Enter Number: "))
+        binary_representation = decimal_to_binary(decimal_number)
+        print(binary_representation)
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+
+if __name__ == "__main__":
+    main()
