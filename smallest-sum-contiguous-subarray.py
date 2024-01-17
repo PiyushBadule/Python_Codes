b957d16f746f9f0351c19ca3080f5c8411fb2506
Piyush Badule <73275216+PiyushBadule@users.noreply.github.com>
2024-01-17 08:37:37 +0530

    Update smallest-sum-contiguous-subarray.py

--- smallest-sum-contiguous-subarray.py
+++ smallest-sum-contiguous-subarray.py
@@ -1,20 +1,34 @@
 import sys
 
-def smallestSumSubarr(arr, n):
-    tmp = sys.maxsize
+def smallest_sum_subarray(arr):
+    """
+    Find the smallest sum of a contiguous subarray in the given array.
 
-    final = sys.maxsize
+    Parameters:
+    arr (list): A list of integers.
 
-    for i in range(n):
-        if (tmp > 0):
-            tmp = arr[i]
+    Returns:
+    int: The smallest sum of a contiguous subarray.
+    """
+    temp_sum = sys.maxsize
+    min_sum = sys.maxsize
+
+    for num in arr:
+        if temp_sum > 0:
+            temp_sum = num
         else:
-            tmp += arr[i]
+            temp_sum += num
 
-        final = min(final, tmp)
+        min_sum = min(min_sum, temp_sum)
 
-    return final
+    return min_sum
 
-arr = [3, -4, 2, -3, -1, 7, -5]
-n = len(arr)
-print("Smallest sum: ", smallestSumSubarr(arr, n))
+def main():
+    """
+    Main function to demonstrate finding the smallest sum of a contiguous subarray.
+    """
+    arr = [3, -4, 2, -3, -1, 7, -5]
+    print("Smallest sum: ", smallest_sum_subarray(arr))
+
+if __name__ == "__main__":
+    main()
